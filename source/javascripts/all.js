8336707381e7807d827cd884215455b719682b0d
Sérgio Fontes <sergiofontes@gmail.com>
2014-02-24 17:45:47 -0300

    Adds popovers, tooltips & expand/collapse behaviors to conference's page

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,3 +1,27 @@
+//= require vendor/bootstrap/tooltip
+//= require vendor/bootstrap/popover
 //= require vendor/bootstrap/transition
 //= require vendor/bootstrap/collapse
 //= require vendor/smooth-scroll
+
+/*
+ * Conference Section
+ */
+
+$('.speaker .talk .more').click(function(e) {
+  e.preventDefault();
+  var $this = $(this);
+
+  var p = $this.parents('.talk').find('p');
+  p.find('.ellipsis').toggle();
+  p.find('.content-remainder').toggleClass('sr-only');
+
+  var newText = $this.data('collapsed-text');
+  $this.data('collapsed-text', $this.text());
+  $this.text(newText);
+});
+
+$('[data-toggle=tooltip]').tooltip();
+$('[data-toggle=popover]').popover().click(function(e) {
+  e.preventDefault()
+});
